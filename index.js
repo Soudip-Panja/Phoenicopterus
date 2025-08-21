c4e537f5036c62880d67b09b766e2c79a2dfe4c1
Soudip Panja <soudip03panja@gmail.com>
2025-08-21 14:07:54 +0530

    feat: Added Delete Routes.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 // Get Routes
 app.get("/", (req, res) => {
@@ -17,25 +17,53 @@ app.get("/contact", (req, res) => {
   res.send("Contact Us page");
 });
 
-// Post routes
+// Post Routes
 const cars = [
-    {id: 1, make: "Toyota", model: "Camry", year: 2022}
-]
-
-app.post("/cars", (req, res) => {
-    const newCar = req.body
-
-    if(!newCar.make || !newCar.model || !newCar.year){
-        res.status(400).json({error: "Make, Model and Year are required."})
-    } else {
-        cars.push(newCar)
-        res.status(201).json({message: "Car added successfully.", car: newCar})
-    }
-})
+  { id: 1, make: "Toyota", model: "Camry", year: 2022 },
+  { id: 2, make: "Honda", model: "Civic", year: 2021 },
+  { id: 3, make: "Ford", model: "Mustang", year: 2022 },
+  { id: 4, make: "Chevrolet", model: "Corvette", year: 2023 },
+  { id: 5, make: "Tesla", model: "Model 3", year: 2023 },
+  { id: 6, make: "Nissan", model: "Altima", year: 2022 },
+  { id: 7, make: "BMW", model: "X5", year: 2023 },
+  { id: 8, make: "Mercedes-Benz", model: "C-Class", year: 2021 },
+  { id: 9, make: "Audi", model: "A4", year: 2022 },
+  { id: 10, make: "Lexus", model: "RX", year: 2023 },
+  { id: 11, make: "Hyundai", model: "Tucson", year: 2021 },
+  { id: 12, make: "Kia", model: "Seltos", year: 2022 },
+  { id: 13, make: "Mazda", model: "CX-5", year: 2023 },
+  { id: 14, make: "Subaru", model: "Outback", year: 2021 },
+  { id: 15, make: "Volkswagen", model: "Golf", year: 2022 },
+];
 
 app.get("/cars", (req, res) => {
-    res.send(cars)
-})
+  res.send(cars);
+});
+
+app.post("/cars", (req, res) => {
+  const newCar = req.body;
+
+  if (!newCar.make || !newCar.model || !newCar.year) {
+    res.status(400).json({ error: "Make, Model and Year are required." });
+  } else {
+    cars.push(newCar);
+    res.status(201).json({ message: "Car added successfully.", car: newCar });
+  }
+});
+
+// Delete Routes
+app.delete("/cars/:id", (req, res) => {
+  const carId = req.params.id;
+
+  const index = cars.findIndex((car) => car.id == carId);
+
+  if (index === -1) {
+    res.status(404).json({ error: "Car not found." });
+  } else {
+    cars.splice(index, 1);
+    res.status(200).json({ message: "Car deleted Successfully." });
+  }
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
